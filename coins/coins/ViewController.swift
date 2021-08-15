6f8ce9c60ef795f85d2df5743d180894f398e609
jrgarrigues <jgarr16@users.noreply.github.com>
2021-08-14 23:51:45 -0700

    initial build for coins

--- coins/coins/ViewController.swift
+++ coins/coins/ViewController.swift
@@ -9,11 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ethPrice: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
+        
+        let url = URL(string: "https://api.coinbase.com/v2/prices/ETH-USD/spot")!
 
+        let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
+            guard let data = data else { return }
+//            print(String(data: data, encoding: .utf8)!)
+//            print()
+
+        do {
+                     
+            var ethPrice = ""
+            // make sure this JSON is in the format we expect
+                     // convert data to json
+                     if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                         // try to read out a dictionary
+                         //print(json)
+                         if let data = json["data"] as? [String:Any] {
+                            self.ethPrice = data["amount"]! as? UILabel
+                             if let prices = data["prices"] as? [[String:Any]] {
+                                 //print(prices)
+                                 let dict = prices[0]
+                                 //print(dict)
+                                 if let price = dict["price"] as? String{
+                                     //print(price)
+                                 }
+                             }
+                         }
+                     }
+                 } catch let error as NSError {
+                     print("Failed to load: \(error.localizedDescription)")
+                
+                 }
+             }
+
+        task.resume()
+    }
 
 }
 
