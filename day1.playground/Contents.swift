f3ae04608e5bbbf4a0841c52b3a2ed4890470413
jrgarrigues <jgarr16@users.noreply.github.com>
2021-08-14 18:42:49 -0700

    initial build for coins

---
+++ day1.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+var greeting = """
+    This line
+    goes over
+    many lines
+    !
+    """
+print(greeting)
+
+var greeting2 = """
+this line
+goes over
+many lines
+too!
+"""
+
+print(greeting2)
+
+
+var greeting3 = """
+    this line i\
+    s the same \
+    but slightly \
+    different.
+    """
+
+print(greeting3)
