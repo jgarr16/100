313e565f6404575980640a19e795effbb7d99c03
jrgarrigues <jgarr16@users.noreply.github.com>
2021-08-16 22:15:31 -0700

    egg timer proj - title lable = done

--- api.playground/Contents.swift
+++ api.playground/Contents.swift
@@ -1,5 +1,5 @@
-import Foundation
-import PlaygroundSupport
+//import Foundation
+//import PlaygroundSupport
 // PlaygroundPage.current.needsIndefiniteExecution = true
 
 //let url = URL(string: "https://api.coinbase.com/v2/prices/ETH-USD/spot")!
@@ -12,9 +12,30 @@ import PlaygroundSupport
 //
 //task.resume()
 
-
-let jsonData = URLSession.shared.dataTask(with: URL(string: "https://api.coinbase.com/v2/prices/ETH-USD/spot")!) {(data, response, error) in
-    print(String(data: data!, encoding: .utf8)!)
-}.resume()
+//
+//let jsonData = URLSession.shared.dataTask(with: URL(string: "https://api.coinbase.com/v2/prices/ETH-USD/spot")!) {(data, response, error) in
+//    print(String(data: data!, encoding: .utf8)!)
+//}.resume()
 //print(jsonData)
+
+import UIKit
+
+// GET, POST, PUT, DELETE, etc.
+
+func apiCall() {
+    guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else {
+        return
+    }
+    
+    var request = URLRequest(url: url)
+    // set the method, body, and any headers that we must use
+    request.httpMethod = "GET"
+    print(request)
+    
+    // make the request
+}
+
+
+apiCall()
+ 
 
