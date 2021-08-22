bac6dd4c1ca18f8d8ea74c99979770b5b2f4da32
jrgarrigues <jgarr16@users.noreply.github.com>
2021-08-21 17:33:19 -0700

    building second TDD course - division

--- division/division/ViewController.swift
+++ division/division/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var div1TextField: UITextField!
+    @IBOutlet weak var div2TextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBAction func calculateButton(_ sender: Any) {
+    }
+  
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
 }
 
